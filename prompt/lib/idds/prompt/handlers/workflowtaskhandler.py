25167c589678f40bf0a023273f85414d52d8e37a
Wen Guan <wguan.icedew@gmail.com>
2026-08-28 10:41:52 +0200

    add streaming mode to start transformer

--- prompt/lib/idds/prompt/handlers/workflowtaskhandler.py
+++ prompt/lib/idds/prompt/handlers/workflowtaskhandler.py
@@ -209,7 +209,8 @@ def _build_task_params(ctx):
     idle_timeout = panda_attrs.get("idle_timeout", 120)
     run_id = ctx.get('run_id')
     verbose_flag = " --verbose" if panda_attrs.get('verbose') else ""
-    executable = f"--run_id {run_id} --idle_timeout {idle_timeout}{verbose_flag}"
+    streaming_mode_flag = " --streaming_mode" if panda_attrs.get('streaming_mode') else ""
+    executable = f"--run_id {run_id} --idle_timeout {idle_timeout}{verbose_flag}{streaming_mode_flag}"
     task_params["jobParameters"] = [
         {
             "type": "constant",
